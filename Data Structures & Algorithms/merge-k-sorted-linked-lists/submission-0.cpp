e8fbcafe78b3b1e29dd31598b575b0ad59476f59
Shubham <76894142+thegeekypenguin@users.noreply.github.com>
2026-08-03 22:32:55 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoSortedLists (ListNode* l1, ListNode* l2){
+        if(!l1) return l2;
+        if(!l2) return l1;
+
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoSortedLists(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoSortedLists(l1, l2->next);
+            return l2;
+        }
+
+        return nullptr;
+    }
+
+    ListNode* partionAndMerge(int start, int end, vector<ListNode*>& lists){
+        if (start > end){
+            return nullptr;
+        }
+
+        if( start == end ){
+            return lists[start]; 
+        }
+        
+        int mid = start + (end - start)/2;
+
+        ListNode* L1 = partionAndMerge(start, mid, lists);
+        ListNode* L2 = partionAndMerge(mid+1, end, lists);
+
+        return mergeTwoSortedLists(L1, L2);
+        
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        if (k==0) return nullptr;
+
+        return partionAndMerge(0, k-1, lists);
+    }
+};
