6cbe3abc97f1af8972132b3f10cac2599fd8ffc2
Shubham <76894142+thegeekypenguin@users.noreply.github.com>
2026-08-26 23:30:08 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.cpp
@@ -0,0 +1,50 @@
+/*                                            Scroll below to see JAVA code also                                    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=3Bp3OVD1EGc
+    Company Tags                : Google, Amazon, Microsoft, Codenation, FactSet, , Atlassian, MakeMyTrip, Streamoid Technologies, Media.net, Airbnb
+    Leetcode Link               : https://leetcode.com/problems/minimum-window-substring/
+*/
+
+/***************************************************************** C++ *****************************************************************/
+//T.C : O(m+n) where m = length of s and n = length of t
+//S.C : O(n)
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.length();
+        map<char, int> mp;
+        
+        for(char &ch : t) {
+            mp[ch]++;
+        }
+        
+        int requiredCount = t.length();
+        int i = 0, j  = 0;
+        int minStart  = 0;
+        int minWindow = INT_MAX;
+        while(j < n) {
+            char ch_j = s[j];
+            if(mp[ch_j] > 0)
+                requiredCount--;
+            
+            mp[ch_j]--;
+            
+            while(requiredCount == 0) { //try to shrink the window
+                if(minWindow > j-i+1) {
+                    minWindow = j-i+1;
+                    minStart  = i;
+                }
+                
+                char ch_i = s[i];
+                mp[ch_i]++;
+                if(mp[ch_i] > 0)
+                    requiredCount++;
+                i++;
+            }
+            
+            j++; //Don't ever forget this :-)
+        }
+        
+        return minWindow == INT_MAX ? "" : s.substr(minStart, minWindow);
+    }
+};
